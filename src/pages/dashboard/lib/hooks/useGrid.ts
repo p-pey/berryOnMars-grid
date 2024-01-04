0be6778864bd9f069d3e427eb713b3f866025331
Pooriya Peyvand <pooriyapeyvandd@gmail.com>
2024-01-04 22:10:51 +0330

    added json server

--- src/pages/dashboard/lib/hooks/useGrid.ts
+++ src/pages/dashboard/lib/hooks/useGrid.ts
@@ -1,15 +1,31 @@
-import { FormEventHandler, useCallback, useRef, useState } from 'react';
+import { FormEventHandler, useCallback, useEffect, useRef, useState } from 'react';
 import { grid } from '../types';
-import staticGridData from '../data/grid.json';
 import { useToggle } from '../../../../hooks';
 import { AgGridReact } from 'ag-grid-react';
+import { FetchApi } from '../../../../services';
+import { toast } from 'sonner';
 
 export default function useGrid() {
   const [isModalOpen, toggleOpenModal] = useToggle(false);
-  const [data, setData] = useState<grid[]>(staticGridData.gridData);
+  const [data, setData] = useState<grid[]>([]);
   const gridRef = useRef<AgGridReact | null>(null);
   const onSearch: FormEventHandler<HTMLInputElement> = useCallback((event) => {
     gridRef.current?.api.setGridOption('quickFilterText', event.currentTarget.value);
+  }, []);
+  const getData = () => {
+    FetchApi({
+      method: 'GET',
+      url: '/gridData',
+    })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        toast.error(error?.message);
+      });
+  };
+  useEffect(() => {
+    getData();
   }, []);
   return {
     get: {
